99c5808e453c309ff10b37ab9b0fbccc25e8f8d7
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-05-16 16:55:06 +0200

    fino a footer

--- JS/main.js
+++ JS/main.js
@@ -4,29 +4,24 @@
 
 const images = [
     {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+        image: 'img/1.jpg',
+        title: 'Se saltiamo è per guardare più lontano',
+        text: 'non ci fermiamo davanti a niente',
     },
     {
-        image: 'img/02.webp',
+        image: 'img/2.jpeg',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     },
     {
-        image: 'img/03.webp',
+        image: 'img/3.jpg',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     },
     {
-        image: 'img/04.webp',
+        image: 'img/4.jpg',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    },
-    {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
 
@@ -58,7 +53,6 @@ function F_indietro() {
     };
     riempiCarosello(index);
 };
-
 
 
 function riempiCarosello(index) {
