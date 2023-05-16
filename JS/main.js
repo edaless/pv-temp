dbd5970f9d2ed29b2ca8e50648820308b6b0fc1b
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-05-16 22:57:45 +0200

    un po' di righe

--- JS/main.js
+++ JS/main.js
@@ -5,23 +5,23 @@
 const images = [
     {
         image: 'img/1.jpg',
-        title: 'Se saltiamo è per guardare più lontano',
-        text: 'non ci fermiamo davanti a niente',
+        title: '',
+        text: '',
     },
     {
         image: 'img/2.jpeg',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+        title: '',
+        text: '',
     },
     {
         image: 'img/3.jpg',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        title: '',
+        text: "",
     },
     {
         image: 'img/4.jpg',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+        title: '',
+        text: '',
     }
 ];
 
